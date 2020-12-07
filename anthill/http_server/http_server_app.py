18b046bfa6bfaabc6418226cfbedb40fdda7505e
artas <mahatma728@gmail.com>
2020-12-07 11:27:19 -0300

    :cyclone:fastapi support (in progress)

--- anthill/http_server/http_server_app.py
+++ anthill/http_server/http_server_app.py
@@ -1,19 +1,48 @@
+import time
+import threading
+import contextlib
+import uvicorn
 from aiohttp import web
+from fastapi import FastAPI
 
 class HTTPServer:
-    def __init__(self, base_app, host: str = None, port: int = None, web_app: web.Application = None):
+    def __init__(self, base_app, host: str = None, port: int = None, web_app = None, web_framework='fastapi'):
         self.app = base_app
-        self.routes = base_app.http
         self.host = host
         self.port = port
+        self.web_framework = web_framework
         if web_app:
             self.web_app = web_app
         else:
-            self.web_app = web.Application()
+            if self.web_framework == 'fastapi':
+                self.web_app = FastAPI()
+            elif self.web_framework == 'aiohttp':
+                self.web_app = web.Application()
 
-    def start_server(self):
-        self._start_server()
+    def start_server(self, uvicorn_app_target: str = None):
+        self._start_server(uvicorn_app_target)
 
-    def _start_server(self):
-        self.web_app.add_routes(self.routes)
-        web.run_app(self.web_app, host=self.host, port=self.port)
+    def _start_server(self, uvicorn_app_target: str = None):
+        if self.web_framework == 'fastapi':
+            config = uvicorn.Config(uvicorn_app_target, host=self.host, port=self.port, log_level="info")
+            self.server = Server(config=config)
+        elif self.web_framework == 'aiohttp':
+            self.web_app.add_routes(self.app.http)
+            web.run_app(self.web_app, host=self.host, port=self.port)
+
+class Server(uvicorn.Server):
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
+
