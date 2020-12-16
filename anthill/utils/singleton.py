089f087f35286a81ac4e1d2c3d661a40a4b0c74f
artas <mahatma728@gmail.com>
2020-12-16 19:59:57 -0300

    :globe_with_meridians: singleton class

--- anthill/utils/singleton.py
+++ anthill/utils/singleton.py
@@ -11,3 +11,10 @@ def singleton(cls):
 
     return getinstance
 
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
