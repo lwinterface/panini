989ab0457973a2ca4f5508397d8f5fd2af740fd6
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-01-18 11:58:02 +0200

    basic tests for anthill ready, implemented web_server test support using requests.Session

--- anthill/testclient.py
+++ anthill/testclient.py
@@ -1,18 +1,84 @@
 import time
 import json
 import random
-from typing import Callable, Optional
+import typing
+from urllib.parse import urljoin
 
+import requests
 from pynats import NATSClient, NATSMessage
 
 from anthill.utils.helper import start_process
+
+
+# Annotations for `Session.request()`
+Cookies = typing.Union[
+    typing.MutableMapping[str, str], requests.cookies.RequestsCookieJar
+]
+Params = typing.Union[bytes, typing.MutableMapping[str, str]]
+DataType = typing.Union[bytes, typing.MutableMapping[str, str], typing.IO]
+TimeOut = typing.Union[float, typing.Tuple[float, float]]
+FileType = typing.MutableMapping[str, typing.IO]
+AuthType = typing.Union[
+    typing.Tuple[str, str],
+    requests.auth.AuthBase,
+    typing.Callable[[requests.Request], requests.Request],
+]
+
+
+class HTTPSessionTestClient(requests.Session):
+    __test__ = False
+
+    def __init__(self, base_url: str = 'http://127.0.0.1:8080'):
+        super(HTTPSessionTestClient, self).__init__()
+        self.base_url = base_url
+
+    def request(  # type: ignore
+            self,
+            method: str,
+            url: str,
+            params: Params = None,
+            data: DataType = None,
+            headers: typing.MutableMapping[str, str] = None,
+            cookies: Cookies = None,
+            files: FileType = None,
+            auth: AuthType = None,
+            timeout: TimeOut = None,
+            allow_redirects: bool = None,
+            proxies: typing.MutableMapping[str, str] = None,
+            hooks: typing.Any = None,
+            stream: bool = None,
+            verify: typing.Union[bool, str] = None,
+            cert: typing.Union[str, typing.Tuple[str, str]] = None,
+            json: typing.Any = None,
+    ) -> requests.Response:
+        url = urljoin(self.base_url, url)
+        return super().request(
+            method,
+            url,
+            params=params,
+            data=data,
+            headers=headers,
+            cookies=cookies,
+            files=files,
+            auth=auth,
+            timeout=timeout,
+            allow_redirects=allow_redirects,
+            proxies=proxies,
+            hooks=hooks,
+            stream=stream,
+            verify=verify,
+            cert=cert,
+            json=json,
+        )
 
 
 class TestClient:
     __test__ = False
 
     def __init__(self,
-                 run_anthill: Callable = lambda callback: None,
+                 run_anthill: typing.Callable = lambda *args, **kwargs: None,
+                 use_web_server: bool = False,
+                 base_web_server_url: str = 'http://127.0.0.1:8080',
                  url: str = 'nats://127.0.0.1:4222',
                  socket_timeout: int = 2,
                  name: str = '__'.join(
@@ -24,8 +90,11 @@ class TestClient:
             name=name,
             socket_timeout=socket_timeout,
         )
-        self.http_session = None
         self.nats_client.connect()
+
+        if use_web_server:
+            self.http_session = HTTPSessionTestClient(base_url=base_web_server_url)
+
         self.anthill_process = None
 
     @staticmethod
@@ -56,9 +125,9 @@ class TestClient:
     def subscribe(
             self,
             topic: str,
-            callback: Callable,
+            callback: typing.Callable,
             queue: str = "",
-            max_messages: Optional[int] = None,
+            max_messages: typing.Optional[int] = None,
     ):
         return self.nats_client.subscribe(
             subject=topic,
---
+++ tests/test_web_server.py
@@ -0,0 +1,43 @@
+import json
+
+from aiohttp import web
+from anthill import app as ant_app
+from anthill.testclient import TestClient
+
+
+def run_anthill():
+
+    app = ant_app.App(
+        service_name='test_web_server',
+        host='127.0.0.1',
+        port=4222,
+        app_strategy='asyncio',
+        web_server=True
+    )
+
+    @app.listen('foo')
+    async def foo(topic, message):
+        message['data'] += 1
+        return message
+
+    @app.http.post('/bar')
+    async def post_listener(request):
+        data = await request.json()
+        data['data'] += 2
+        return web.json_response(data)
+
+    app.start()
+
+
+# provide parameter for using web_server - use_web_server; for waiting for web_server setup - sleep_time;
+client = TestClient(run_anthill=run_anthill, use_web_server=True).start(sleep_time=1)
+
+
+def test_request_and_post():
+    response = {'data': 0}
+    for i in range(5):
+        response = client.request('foo', message={'data': response['data']})
+        response = client.http_session.post('bar', data=json.dumps({'data': response['data']}))
+        assert response.status_code == 200, response.text
+        response = response.json()
+        assert response['data'] == 3 * (i + 1)
