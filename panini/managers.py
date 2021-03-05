0c855e9e9d9e293a5c61197f5fab550f93061587
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-03-05 19:06:23 +0200

    quick fix typo

--- panini/managers.py
+++ panini/managers.py
@@ -22,6 +22,7 @@ class _EventManager:
         self, subject: list or str, validator: type = None, dynamic_subscription=False
     ):
         def wrapper(function):
+            function = _MiddlewareManager._wrap_function_by_middleware(function, "listen")
             function = _EventManager.wrap_function_by_validator(function, validator)
             if type(subject) is list:
                 for t in subject:
@@ -167,8 +168,11 @@ class _MiddlewareManager:
         "listen_request_middleware": [],
     }
 
-    def add_middleware(self, middleware_cls: Type[Middleware], *args, **kwargs):
-        assert issubclass(middleware_cls, Middleware), "Each custom middleware class must be a subclass of Middleware"
+    @staticmethod
+    def add_middleware(middleware_cls: Type[Middleware], *args, **kwargs):
+        assert issubclass(
+            middleware_cls, Middleware
+        ), "Each custom middleware class must be a subclass of Middleware"
         high_priority_functions = (
             "send_publish",
             "listen_publish",
@@ -186,33 +190,122 @@ class _MiddlewareManager:
         middleware_obj = middleware_cls(*args, **kwargs)
         for function_name in high_priority_functions:
             if function_name in middleware_cls.__dict__:
-                self.MIDDLEWARE[f"{function_name}_middleware"].append(
+                _MiddlewareManager.MIDDLEWARE[f"{function_name}_middleware"].append(
                     getattr(middleware_obj, function_name)
                 )
 
         if "send_any" in middleware_cls.__dict__:
             if "send_publish" not in middleware_cls.__dict__:
-                self.MIDDLEWARE[f"send_publish_middleware"].append(
+                _MiddlewareManager.MIDDLEWARE[f"send_publish_middleware"].append(
                     middleware_obj.send_any
                 )
 
             if "send_request" not in middleware_cls.__dict__:
-                self.MIDDLEWARE[f"send_request_middleware"].append(
+                _MiddlewareManager.MIDDLEWARE[f"send_request_middleware"].append(
                     middleware_obj.send_any
                 )
 
         if "listen_any" in middleware_cls.__dict__:
             if "listen_publish" not in middleware_cls.__dict__:
-                self.MIDDLEWARE[f"listen_publish_middleware"].append(
+                _MiddlewareManager.MIDDLEWARE[f"listen_publish_middleware"].append(
                     middleware_obj.listen_any
                 )
 
             if "listen_request" not in middleware_cls.__dict__:
-                self.MIDDLEWARE[f"listen_request_middleware"].append(
+                _MiddlewareManager.MIDDLEWARE[f"listen_request_middleware"].append(
                     middleware_obj.listen_any
                 )
 
     @staticmethod
-    def wrap_function_by_middleware():
-        # TODO: implement wrapping callbacks and functions
-        pass
+    def _wrap_function_by_middleware(
+        function: Callable, function_type: str
+    ) -> Callable:
+        """
+        function: Callable - function to wrap
+        function_type: str - one of the ["listen", "publish", "request"]
+        """
+        assert function_type in (
+            "listen",
+            "publish",
+            "request",
+        ), "function type must be in (`listen`, `publish`, `request`)"
+
+        def wrap_function_by_send_middleware(
+            func: Callable, single_middleware
+        ) -> Callable:
+            def next_wrapper(subject: str, message, **kwargs):
+                return single_middleware(subject, message, func, **kwargs)
+
+            async def aio_next_wrapper(subject: str, message, **kwargs):
+                return await single_middleware(subject, message, func, **kwargs)
+
+            if asyncio.iscoroutinefunction(single_middleware):
+                return aio_next_wrapper
+            else:
+                return next_wrapper
+
+        def wrap_function_by_listen_middleware(
+            func: Callable, single_middleware
+        ) -> Callable:
+            def next_wrapper(msg):
+                return single_middleware(msg, func)
+
+            async def aio_next_wrapper(msg):
+                return await single_middleware(msg, func)
+
+            if asyncio.iscoroutinefunction(single_middleware):
+                return aio_next_wrapper
+            else:
+                return next_wrapper
+
+        def build_middleware_wrapper(
+            func: Callable, middleware_key: str, wrapper: Callable
+        ) -> Callable:
+            for middleware in _MiddlewareManager.MIDDLEWARE[middleware_key]:
+                func = wrapper(func, middleware)
+            return func
+
+        if function_type == "publish":
+            return build_middleware_wrapper(
+                function, "send_publish_middleware", wrap_function_by_send_middleware
+            )
+
+        elif function_type == "request":
+            return build_middleware_wrapper(
+                function, "send_request_middleware", wrap_function_by_send_middleware
+            )
+
+        else:
+            if (
+                len(_MiddlewareManager.MIDDLEWARE["listen_publish_middleware"]) == 0
+                and len(_MiddlewareManager.MIDDLEWARE["listen_request_middleware"]) == 0
+            ):
+                return function
+
+            function_listen_publish = build_middleware_wrapper(
+                function,
+                "listen_publish_middleware",
+                wrap_function_by_listen_middleware,
+            )
+            function_listen_request = build_middleware_wrapper(
+                function,
+                "listen_request_middleware",
+                wrap_function_by_listen_middleware,
+            )
+
+            def listen_wrapper(msg) -> Callable:
+                if msg.reply == "":
+                    return function_listen_publish(msg)
+                else:
+                    return function_listen_request(msg)
+
+            async def aio_listen_wrapper(msg) -> Callable:
+                if msg.reply == "":
+                    return await function_listen_publish(msg)
+                else:
+                    return await function_listen_request(msg)
+
+            if asyncio.iscoroutinefunction(function):
+                return aio_listen_wrapper
+            else:
+                return listen_wrapper
