df3091bc9d92ccf0c77b37783720211467775ef5
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-03-05 18:28:35 +0200

    MiddlewareManager completed with tests

--- tests/test_middleware_manager.py
+++ tests/test_middleware_manager.py
@@ -1,13 +1,16 @@
+import asyncio
+
 import pytest
 
 from panini import app as panini_app
+from panini.managers import _MiddlewareManager
 from panini.middleware import Middleware
 
 
 class FooMiddleware(Middleware):
     async def send_publish(self, subject: str, message, publish_func, **kwargs):
         """FooMiddleware send_publish"""  # docstring is important for testing
-        pass  # implementation is not important for middleware_dict_factory
+        pass  # implementation is not important for some of these tests
 
     async def listen_publish(self, msg, callback):
         """FooMiddleware listen_publish"""
@@ -54,6 +57,34 @@ class IncorrectMiddleware:  # not inherited from Middleware
         pass
 
 
+class IncSyncMiddleware(Middleware):
+    def send_any(self, subject: str, message, send_func, **kwargs):
+        message["data"] += 1
+        send_func(subject, message, **kwargs)
+        message["data"] += 3
+
+
+class DecSyncMiddleware(Middleware):
+    def listen_any(self, msg, callback):
+        msg.data -= 1
+        callback(msg)
+        msg.data -= 3
+
+
+class IncMiddleware(Middleware):
+    async def send_any(self, subject: str, message, send_func, **kwargs):
+        message["data"] += 1
+        await send_func(subject, message, **kwargs)
+        message["data"] += 3
+
+
+class DecMiddleware(Middleware):
+    async def listen_any(self, msg, callback):
+        msg.data -= 1
+        await callback(msg)
+        msg.data -= 3
+
+
 @pytest.fixture
 def app():
     app = panini_app.App(
@@ -61,7 +92,7 @@ def app():
         host="127.0.0.1",
         port=4222,
     )
-    app.MIDDLEWARE = {
+    _MiddlewareManager.MIDDLEWARE = {
         "send_publish_middleware": [],
         "listen_publish_middleware": [],
         "send_request_middleware": [],
@@ -72,7 +103,7 @@ def app():
 
 def test_foo_middleware(app):
     app.add_middleware(FooMiddleware)
-    middleware_dict = app.MIDDLEWARE
+    middleware_dict = _MiddlewareManager.MIDDLEWARE
     assert len(middleware_dict["send_publish_middleware"]) == 1
     assert len(middleware_dict["send_request_middleware"]) == 0
     assert len(middleware_dict["listen_publish_middleware"]) == 1
@@ -94,7 +125,7 @@ def test_foo_middleware(app):
 
 def test_bar_middleware(app):
     app.add_middleware(BarMiddleware)
-    middleware_dict = app.MIDDLEWARE
+    middleware_dict = _MiddlewareManager.MIDDLEWARE
 
     assert len(middleware_dict["send_publish_middleware"]) == 1
     assert len(middleware_dict["send_request_middleware"]) == 1
@@ -121,7 +152,7 @@ def test_bar_middleware(app):
 
 def test_foo_bar_middleware(app):
     app.add_middleware(FooBarMiddleware, "foo", bar="bar")
-    middleware_dict = app.MIDDLEWARE
+    middleware_dict = _MiddlewareManager.MIDDLEWARE
     assert len(middleware_dict["send_publish_middleware"]) == 1
     assert len(middleware_dict["send_request_middleware"]) == 1
     assert len(middleware_dict["listen_publish_middleware"]) == 0
@@ -146,7 +177,7 @@ def test_multiple_middlewares_order(app):
     app.add_middleware(FooMiddleware)
     app.add_middleware(BarMiddleware)
     app.add_middleware(FooBarMiddleware, "foo", bar="bar")
-    middleware_dict = app.MIDDLEWARE
+    middleware_dict = _MiddlewareManager.MIDDLEWARE
     assert len(middleware_dict["send_publish_middleware"]) == 3
     assert len(middleware_dict["send_request_middleware"]) == 2
     assert len(middleware_dict["listen_publish_middleware"]) == 2
@@ -195,3 +226,127 @@ def test_incorrect_middlewares(app):
         app.add_middleware(
             FooBarMiddleware
         )  # not enough parameters for FooBarMiddleware __init__() method
+
+
+def test_wrap_function_by_middleware_inc_sync(app):
+    def foo(subject, message, **kwargs):
+        assert message["data"] == 2
+        message["data"] += 2
+
+    app.add_middleware(IncSyncMiddleware)
+    for send in ("publish", "request"):
+        wrapped_function = app._wrap_function_by_middleware(foo, send)
+        temp_message = {"data": 1}
+        wrapped_function("", temp_message)
+        assert temp_message["data"] == 7
+
+
+def test_wrap_function_by_middleware_dec_sync(app):
+    class Bar:
+        data = 5
+        reply = ""
+
+    def bar(msg):
+        assert msg.data == 4
+        msg.data -= 2
+
+    app.add_middleware(DecSyncMiddleware)
+    wrapped_function = app._wrap_function_by_middleware(bar, "listen")
+    temp_message = Bar()
+    wrapped_function(temp_message)
+    assert temp_message.data == -1
+
+
+def test_wrap_function_by_middleware_inc(app):
+    async def foo(subject, message, **kwargs):
+        assert message["data"] == 2
+        message["data"] += 2
+
+    app.add_middleware(IncMiddleware)
+    loop = asyncio.get_event_loop()
+
+    for send in ("publish", "request"):
+        wrapped_function = app._wrap_function_by_middleware(foo, send)
+        temp_message = {"data": 1}
+        loop.run_until_complete(wrapped_function("", temp_message))
+        assert temp_message["data"] == 7
+
+
+def test_wrap_function_by_middleware_dec(app):
+    class Bar:
+        data = 5
+        reply = ""
+
+    async def bar(msg):
+        assert msg.data == 4
+        msg.data -= 2
+
+    loop = asyncio.get_event_loop()
+
+    app.add_middleware(DecMiddleware)
+    wrapped_function = app._wrap_function_by_middleware(bar, "listen")
+    temp_message = Bar()
+    loop.run_until_complete(wrapped_function(temp_message))
+    assert temp_message.data == -1
+
+
+def test_wrap_function_by_middleware_inc_dec_sync(app):
+    class Bar:
+        data = 5
+        reply = "someone"
+
+    def foo(subject, message, **kwargs):
+        assert message["data"] == 3
+        message["data"] += 2
+
+    def bar(msg):
+        assert msg.data == 3
+        msg.data -= 2
+
+    app.add_middleware(IncSyncMiddleware)
+    app.add_middleware(IncSyncMiddleware)
+    app.add_middleware(DecSyncMiddleware)
+    app.add_middleware(DecSyncMiddleware)
+
+    for send in ("publish", "request"):
+        wrapped_function = app._wrap_function_by_middleware(foo, send)
+        temp_message = {"data": 1}
+        wrapped_function("", temp_message)
+        assert temp_message["data"] == 11
+
+    wrapped_function = app._wrap_function_by_middleware(bar, "listen")
+    temp_message = Bar()
+    wrapped_function(temp_message)
+    assert temp_message.data == -5
+
+
+def test_wrap_function_by_middleware_inc_dec(app):
+    class Bar:
+        data = 5
+        reply = "someone"
+
+    async def foo(subject, message, **kwargs):
+        assert message["data"] == 3
+        message["data"] += 2
+
+    async def bar(msg):
+        assert msg.data == 3
+        msg.data -= 2
+
+    app.add_middleware(IncMiddleware)
+    app.add_middleware(IncMiddleware)
+    app.add_middleware(DecMiddleware)
+    app.add_middleware(DecMiddleware)
+
+    loop = asyncio.get_event_loop()
+
+    for send in ("publish", "request"):
+        wrapped_function = app._wrap_function_by_middleware(foo, send)
+        temp_message = {"data": 1}
+        loop.run_until_complete(wrapped_function("", temp_message))
+        assert temp_message["data"] == 11
+
+    wrapped_function = app._wrap_function_by_middleware(bar, "listen")
+    temp_message = Bar()
+    loop.run_until_complete(wrapped_function(temp_message))
+    assert temp_message.data == -5
