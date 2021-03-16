df11c7fdb59c775c6debc30b9686a8734563a665
oleksii-v <Akxzv123asdfJDSVB8234>
2021-03-16 14:45:32 +0200

    samples has been fixed

--- examples/emulator/writer.py
+++ examples/emulator/writer.py
@@ -1,11 +1,11 @@
 import asyncio
 from datetime import datetime
 
-from panini.emulator.event_storage_middleware import EventStorageMiddleware
+from panini.emulator.writer_emulator_middleware import WriterEmulatorMiddleware
 from panini.app import App
 
 app = App(
-    service_name="storage_example",
+    service_name="writer",
     host="127.0.0.1",
     port=4222
 )
@@ -27,46 +27,29 @@ async def response(message):
         app.logger.exception(message.subject)
 
 
-@app.timer_task(interval=2)
-async def publish():
-    subject = "store.listen"
-    try:
-        await app.publish(subject, {"data": "publish.data"})
-    except Exception as ex:
-        app.logger.exception(subject)
-
-
-@app.task(interval=3)
-async def request():
-    subject = "store.request"
-    try:
-        await app.request(subject, {"data": "request.data"})
-    except Exception as ex:
-        app.logger.exception(subject)
-
-
 @app.task()
 async def request_task():
     for i in range(10):
         subject = "store.request"
         try:
-            await app.request(subject, {"data": "request.data"})
+            await app.request(subject, {"data": f"request.data.{i}"})
             await asyncio.sleep(3)
         except Exception as ex:
             app.logger.exception(subject)
-
+    print("finish request")
 
 @app.task()
 async def publish_task():
     for i in range(10):
         subject = "store.listen"
         try:
-            await app.publish(subject, {"data": "publish.data"})
+            await app.publish(subject, {"data": f"publish.data.{i}"})
             await asyncio.sleep(2)
         except Exception as ex:
             app.logger.exception(subject)
-
+    print("finish publish")
 
 if __name__ == "__main__":
-    app.add_middleware(EventStorageMiddleware, filename=f"resources/events.{datetime.now().strftime('%Y-%m-%d-%H:%M')}.jsonl")
+    events_filename = f"resources/events.{app.service_name}.{datetime.now().strftime('%Y-%m-%d-%H:%M')}.jsonl"
+    app.add_middleware(WriterEmulatorMiddleware, filename=events_filename)
     app.start()
