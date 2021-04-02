43c07c082854e033d9c5b80ebb950b475ca3f24a
Daniel <d.tuytuyshkin@gmail.com>
2021-04-02 13:39:46 +0300

    hotfix for test_wss - added sleep waiting for websockets

--- tests/test_wss.py
+++ tests/test_wss.py
@@ -1,5 +1,6 @@
 import uuid
 import json
+import time
 
 import pytest
 from aiohttp import web
@@ -59,6 +60,7 @@ def client():
 
 def test_wss_bridge(client):
     print("Before connect")
+    time.sleep(1)
     client.websocket_session.connect("ws://127.0.0.1:1111/test_wss/stream")
     print("Connected")
     subscribe_message = {
