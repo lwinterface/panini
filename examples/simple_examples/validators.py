9298638668471916ef3465557597bd2b9bd2e413
artas728 <andrew.volotscov@gmail.com>
2022-02-22 14:34:02 +0200

    added validation error callback

--- examples/simple_examples/validators.py
+++ examples/simple_examples/validators.py
@@ -28,7 +28,7 @@ class TestValidator(Validator):
 
 
 message = {
-    "key1": "value1",
+    "key1": ["value1",],
     "key2": 2,
     "key3": 3.0,
     "key4": [1, 2, 3, 4],
@@ -50,7 +50,11 @@ async def publish_periodically():
         await app.publish(subject="some.publish.subject", message=message)
 
 
-@app.listen("some.publish.subject", validator=TestValidator)
+def validation_error_cb(msg, error):
+    return {"success": False, "error": "validation_error_cb"}
+
+
+@app.listen("some.publish.subject", validator=TestValidator, validation_error_cb=validation_error_cb)
 async def requests_listener(msg):
     log.info(f"got message {msg.data}")
     await asyncio.sleep(1)
