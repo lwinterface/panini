b1ab055523503dab4d16a8d7a6c51ed971bf648e
artas728 <andrew.volotscov@gmail.com>
2022-04-01 15:38:56 +0300

    add test for long task

--- tests/helper.py
+++ tests/helper.py
@@ -8,6 +8,7 @@ class Global:
         self.public_variable = 0
         self.another_variable = 0
         self.additional_variable = 0
+        self.list_variable = []
 
 
 testing_directory_path = pathlib.Path(__file__).parent.absolute()
