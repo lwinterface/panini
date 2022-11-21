8a8d1c3aa59dd2ada5625112955ded34fb393843
artas728 <andrew.volotscov@gmail.com>
2022-11-21 09:48:57 -0800

    update tests

--- tests/test_serializer_callable.py
+++ tests/test_serializer_callable.py
@@ -1,39 +1,45 @@
 import pytest
 
-from panini.exceptions import ValidationError
+from panini.exceptions import MessageSchemaError
 from panini.test_client import TestClient
 from panini import app as panini_app
-from panini.validator import Validator, Field
 from .helper import Global
+
 
 
 def run_panini():
     app = panini_app.App(
-        service_name="test_validator",
+        service_name="test_serializer_callable",
         host="127.0.0.1",
         port=4222,
         logger_in_separate_process=False,
     )
 
-    class DataValidator(Validator):
-        data = Field(type=int)
+    def callable_validator(**message):
+        if type(message) is not dict:
+            raise MessageSchemaError("type(data) is not dict")
+        if "data" not in message:
+            raise MessageSchemaError("'data' not in message")
+        if type(message["data"]) is not int:
+            raise MessageSchemaError("type(message['data']) is not int")
+        if message["data"] < 0:
+            raise MessageSchemaError(f"Value of field 'data' is {message['data']} that negative")
+        message["data"] += 1
+        return message
 
-    @app.listen("test_validator.foo", validator=DataValidator)
+    @app.listen("test_validator.foo", data_type=callable_validator)
     async def publish(msg):
         return {"success": True}
 
-    def validation_error_cb(msg, error):
-        return {"success": False, "error": "validation_error_cb"}
-
-    @app.listen("test_validator.foo-with-error-cb", validator=DataValidator, validation_error_cb=validation_error_cb)
+    @app.listen("test_validator.foo-with-error-cb", data_type=callable_validator)
     async def publish(msg):
         return {"success": True}
 
     @app.listen("test_validator.check")
     async def check(msg):
         try:
-            DataValidator.validated_message(msg.data)
-        except ValidationError:
+            callable_validator(**msg.data)
+        except MessageSchemaError:
             return {"success": False}
 
         return {"success": True}
@@ -64,20 +70,20 @@ def test_correct_message(client):
 
 
 def test_request_with_correct_message(client):
-    response = client.request("test_validator.foo", {"data": "string"})
-    assert response["success"] is False
+    response = client.request("test_validator.foo", {"data": 1})
+    assert response["success"] is True
 
 
 def test_request_with_incorrect_message(client):
-    response = client.request("test_validator.foo", {"data": 1})
+    response = client.request("test_validator.foo", {"data": "string"})
     assert (
-        response["success"] is True
-    )  # both should be success = True, validator do not stop the request
+        response["success"] is False
+    )
 
 
 def test_request_with_incorrect_message_error_cb(client):
     response = client.request("test_validator.foo-with-error-cb", {"notdata": "string"})
     assert response["success"] is False
     assert "error" in response
-    assert "validation_error_cb" in response["error"]
+    assert "MessageSchemaError" in response["error"]
 
