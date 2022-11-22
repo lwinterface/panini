759b4c9596a665cdd041e933ae6f25d3488f5374
artas728 <andrew.volotscov@gmail.com>
2022-11-21 17:18:00 -0800

    examples update

--- examples/emulator/listener_app.py
+++ examples/emulator/listener_app.py
@@ -1,7 +1,7 @@
 from panini.app import App
 
 from panini.middleware.reader_emulator_middleware import ReaderEmulatorMiddleware
-from panini.managers.nats_client import Msg
+from nats.aio.msg import Msg
 
 app = App(service_name="listener", host="127.0.0.1", port=4222)
 
@@ -25,7 +25,7 @@ async def response(message: Msg):
 
 if __name__ == "__main__":
     folder = "resources"
-    filename = "resources/events.listener.2021-03-22-16-30-18.jsonl"
+    filename = "./resources/events.listener.2021-03-22-16-30-18.jsonl"
     # app.add_middleware(WriterEmulatorMiddleware, folder=folder)
     app.add_middleware(ReaderEmulatorMiddleware, filename=filename)
     # app.add_middleware(ReaderEmulatorMiddleware, prefix='prefix', run_emulator=False)
