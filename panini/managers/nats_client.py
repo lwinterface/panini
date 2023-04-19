93281a47c97358f46f3de26d4dd15bf6286d7d73
artas728 <mahatma728@gmail.com>
2023-04-19 06:34:09 -0700

    request_sync minor fix

--- panini/managers/nats_client.py
+++ panini/managers/nats_client.py
@@ -261,7 +261,7 @@ class NATSClient:
         return self.loop.run_until_complete(
             self.request(subject, message, timeout, response_data_type, headers, *args,
             **kwargs
-        )
+        ))
 
     def request_from_another_thread_sync(
             self,
