e64df1cb411a00dc3d8a11f55f9653b0a0d019a4
Igor Khvan <i2gor87@gmail.com>
2023-12-11 21:11:15 +0900

    add ability to ignore subjects via config. Updated example

--- examples/tracing_middleware_compose/receiver/app/main.py
+++ examples/tracing_middleware_compose/receiver/app/main.py
@@ -3,11 +3,15 @@ import asyncio
 import yaml
 from nats.aio.msg import Msg
 from panini import app as panini_app
-from panini.middleware.tracing_middleware import TracingMiddleware, SpanConfig, TracingEvent
+from panini.middleware.tracing_middleware import (
+    TracingMiddleware,
+    SpanConfig,
+    TracingEvent,
+)
 
 app = panini_app.App(
     servers=["nats://nats-server:4222"],
-    service_name='receiver-nats',
+    service_name="receiver-nats",
 )
 
 log = app.logger
@@ -21,18 +25,10 @@ async def default_auto_tracing(msg: Msg):
 @app.listen("test.tracing.middleware.with.events")
 async def listen_with_events(msg: Msg):
     event = TracingEvent(
-        event_name='tracing_event_01',
-        event_data={
-            "took_ms": 15,
-            "request_from": "frontend",
-            "request_type": "POST"
-        }
+        event_name="tracing_event_01",
+        event_data={"took_ms": 15, "request_from": "frontend", "request_type": "POST"},
     )
-    return {
-        "success": True,
-        "data": [1, 2, 3, 4, 5],
-        "tracing_events": [event]
-    }
+    return {"success": True, "data": [1, 2, 3, 4, 5], "tracing_events": [event]}
 
 
 @app.listen("test.tracing.middleware.with.exceptions")
@@ -46,10 +42,7 @@ async def restrict_tracing(msg: Msg):
 
 
 receiver_span_config = SpanConfig(
-    span_name="receiver_span",
-    span_attributes={
-        "receiver_key": "receiver_value"
-    }
+    span_name="receiver_span", span_attributes={"receiver_key": "receiver_value"}
 )
 
 
@@ -66,14 +59,16 @@ from panini.middleware.tracing_middleware import register_trace
 async def job_2(price: int):
     await asyncio.sleep(1)  # heavy work
     if price >= 1:
-        await app.publish("test.tracing.inside.microservice.success", {}, use_current_span=True)
+        await app.publish(
+            "test.tracing.inside.microservice.success", {}, use_current_span=True
+        )
         return True
     else:
         await app.publish("test.tracing.inside.microservice.fail", {})
         return False
 
 
-@register_trace(span_name='job1')
+@register_trace(span_name="job1")
 async def job_1(msg: Msg):
     return await job_2(msg.data.get("price"))
 
@@ -84,13 +79,42 @@ async def tracing_multiple_layers(msg: Msg):
     return {"result": result}
 
 
+@app.listen("ignore.this.subject")
+async def ignore_this_subect(msg: Msg):
+    return {"result": "This should not be in tracing"}
+
+
+@app.listen("ignore.this.subject.wildcard.anything")
+async def ignore_this_subject_wildcard(msg: Msg):
+    return {"result": "This should not be in tracing"}
+
+
+@app.listen("ignore.this.subject.and.more.>")
+async def ignore_this_subject_and_more(msg: Msg):
+    return {"result": "This should not be in tracing"}
+
+
+@app.listen("ignore.only.listen.subject")
+async def ignore_only_listen_subject(msg: Msg):
+    return {"result": "This should not be in tracing"}
+
+
+@app.listen("ignore.only.send.subject")
+async def ignore_only_send_subject(msg: Msg):
+    return {"result": "Only listen should be in tracing"}
+
+
+@app.listen("check.other.subjects")
+async def check_other_subjects(msg: Msg):
+    return {"result": "Both send and listen should be in tracing"}
+
+
 # mimic config loading from yaml file
-with open('tracing_middleware_config.yaml', 'r') as file:
+with open("tracing_middleware_config.yaml", "r") as file:
     tracing_config = yaml.load(file, Loader=yaml.FullLoader)
 
 tracing_config["service_name"] = "receiver_test"
 
 if __name__ == "__main__":
-    app.add_middleware(TracingMiddleware,
-                       tracing_config=tracing_config)
+    app.add_middleware(TracingMiddleware, tracing_config=tracing_config)
     app.start()
--- examples/tracing_middleware_compose/sender/app/main.py
+++ examples/tracing_middleware_compose/sender/app/main.py
@@ -2,11 +2,15 @@ import yaml
 from nats.aio.msg import Msg
 
 from panini import app as panini_app
-from panini.middleware.tracing_middleware import SpanConfig, TracingMiddleware, TracingEvent
+from panini.middleware.tracing_middleware import (
+    SpanConfig,
+    TracingMiddleware,
+    TracingEvent,
+)
 
 app = panini_app.App(
     servers=["nats://nats-server:4222"],
-    service_name='sender-nats',
+    service_name="sender-nats",
 )
 
 log = app.logger
@@ -30,27 +34,29 @@ async def restrict_tracing():
 async def custom_span_tracing():
     sender_span_config = SpanConfig(
         span_name="sender_span",
-        span_attributes={
-            "sender_key": "sender_value",
-            "publish": "true"
-        }
+        span_attributes={"sender_key": "sender_value", "publish": "true"},
     )
-    await app.publish("test.tracing.middleware.custom_config", {}, span_config=sender_span_config)
+    await app.publish(
+        "test.tracing.middleware.custom_config", {}, span_config=sender_span_config
+    )
     sender_span_config = SpanConfig(
         span_name="sender_span",
-        span_attributes={
-            "sender_key": "sender_value",
-            "publish": "false"
-        }
+        span_attributes={"sender_key": "sender_value", "publish": "false"},
     )
-    await app.request("test.tracing.middleware.custom_config", {}, span_config=sender_span_config)
+    await app.request(
+        "test.tracing.middleware.custom_config", {}, span_config=sender_span_config
+    )
     return {"result": True}
 
 
 @app.task()
 async def tracing_with_events():
     event = TracingEvent()
-    await app.publish("test.tracing.middleware.with.events", {"data": [1, 2, 3, 4, 5]}, tracing_events=[event])
+    await app.publish(
+        "test.tracing.middleware.with.events",
+        {"data": [1, 2, 3, 4, 5]},
+        tracing_events=[event],
+    )
 
 
 @app.task()
@@ -60,7 +66,7 @@ async def exception_tracing_in_listen_function():
 
 @app.task()
 async def exception_tracing_in_send_function():
-    await app.publish('test', 1)
+    await app.publish("test", 1)
 
 
 ######################## EXAMPLE OF TRACING WITHIN MICROSERVICE ########################
@@ -72,20 +78,44 @@ async def tracing_within_microservice():
 
 @app.listen("test.tracing.inside.microservice.*")
 async def success_or_fail_result(msg: Msg):
-    _, _, _, _, success = msg.subject.split('.')
+    _, _, _, _, success = msg.subject.split(".")
     if success == "success":
         log.info("Got success message")
     else:
         log.info("Got fail message")
 
 
+@app.task()
+async def ignoring_subjects_via_config():
+    await app.publish(
+        "ignore.this.subject", {"result": "This should not be in tracing"}
+    )
+    await app.publish(
+        "ignore.this.subject.wildcard.anything",
+        {"result": "This should not be in tracing"},
+    )
+    await app.publish(
+        "ignore.this.subject.and.more.of.it",
+        {"result": "This should not be in tracing"},
+    )
+    await app.publish(
+        "ignore.this.subject.and.more.of.it",
+        {"result": "Only send should be in tracing"},
+    )
+    await app.publish(
+        "ignore.only.send.subject", {"result": "Only listen should be in tracing"}
+    )
+    await app.publish(
+        "check.other.subjects", {"result": "Both send and listen should be in tracing"}
+    )
+
+
 # mimic config loading from yaml file
-with open('tracing_middleware_config.yaml', 'r') as file:
+with open("tracing_middleware_config.yaml", "r") as file:
     tracing_config = yaml.load(file, Loader=yaml.FullLoader)
 
 tracing_config["service_name"] = "sender_test"
 
 if __name__ == "__main__":
-    app.add_middleware(TracingMiddleware,
-                       tracing_config=tracing_config)
+    app.add_middleware(TracingMiddleware, tracing_config=tracing_config)
     app.start()
